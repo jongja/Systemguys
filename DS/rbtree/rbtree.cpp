207f14932b6edfeda5aa586a0682b26fe393a4f9
jongja <whwhdgus94@naver.com>
2020-06-21 15:10:29 +0900

    make new ds : rbtree

---
+++ DS/rbtree/rbtree.cpp
@@ -0,0 +1,30 @@
+﻿/* Red Black Tree
+ *
+ * Author : Jongja & JaeHyuk
+ * Created by Systemguys 2020
+ * Copyright © 2020 Systemguys All rights reserved.
+ *
+ */
+
+#include "rbtree.h"
+
+void RedBlackTree::rbtree_clear(rbnode* cur)
+{
+	if (cur->left) rbtree_clear(cur->left);
+	if (cur->right) rbtree_clear(cur->right);
+	delete cur;
+}
+
+RedBlackTree::RedBlackTree()
+{
+	this->root = NULL;
+	this->size = 0;
+}
+
+RedBlackTree::~RedBlackTree()
+{
+	rbnode* walker = this->root;
+	if (walker->left) rbtree_clear(walker->left);
+	if (walker->right) rbtree_clear(walker->right);
+	delete walker;
+}
---
+++ DS/rbtree/rbtree.h
@@ -0,0 +1,28 @@
+﻿/* Red Black Tree
+ *
+ * Author : Jongja & JaeHyuk
+ * Created by Systemguys 2020
+ * Copyright © 2020 Systemguys All rights reserved.
+ *
+ */
+#pragma once
+#include <iostream>
+
+typedef struct RedBlackTreeNode
+{
+	int color;
+	int data; // this variable "data" can be changed for any purpose.
+	struct RedBlackTreeNode* left;
+	struct RedBlackTreeNode* right;
+}rbnode;
+
+typedef class RedBlackTree {
+public:
+	int size;
+	rbnode* root;
+	RedBlackTree();
+	~RedBlackTree();
+	void rbtree_clear(rbnode* cur);
+	void rbtree_insert(int data);
+	void rbtree_delete(int data);
+}rbtree;
