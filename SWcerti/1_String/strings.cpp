a793fc3496c6f1a61c413b67df6387323556d043
jongja <whwhdgus94@naver.com>
2020-08-05 22:49:41 +0900

    Delete few lines

--- SWcerti/1_String/strings.cpp
+++ SWcerti/1_String/strings.cpp
@@ -4,11 +4,6 @@
  *
 */
 
-#include <iostream>
-using namespace std;
-
-#define STR_MAX_SIZE 100
-
 int _strlen(char* str) { // Simple string length function
     int len = 0;
     while (*(str++)) len++;
@@ -29,19 +24,4 @@ int _strcmp(char* str1, char* str2) { // Simple string compare function
         str1++; str2++;
     }
     return *(str2) == '\0' ? 0 : -1;
-}
-
-int main(void) {
-	char str1[] = "Hi, we are systemguys! nice to meet you!";
-    char str2[] = "Hi, we are systemguys! nice to meat you!";
-    char str3[STR_MAX_SIZE] = { 0 };
-    // _strlen test
-    cout << "Size of str1 --> " << _strlen(str1) << endl; 
-    cout << "Size of str2 --> " << _strlen(str2) << endl;
-    // _strcpy test
-    _strcpy(str3, str1);
-    cout << str3 << endl;
-    // _strcmp test
-    cout << _strcmp(str1, str2) << endl;
-	return 0;
 }
