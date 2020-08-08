401cc8191b0c411d00a9615c6190ecca8b912b5f
Jongja <whwhdgus94@naver.com>
2020-08-08 17:44:41 +0900

    complete heap

--- SWcerti/6_Heap/heap.cpp
+++ SWcerti/6_Heap/heap.cpp
@@ -22,9 +22,17 @@ void heap_push(int val){
 
 int heap_pop(void){
 	if(!heap_size) return;
+	int res = heap[1];
+
 	int e = heap[heap_size--];
 	int cur = 1, child = 2;
 	while(child <= heap_size){
 		if(child < heap_size && heap[child] < heap[child + 1]) child++;
-		if(heap[child] >= e) break;
-  // Continue..
+		if(heap[child] <= e) break;
+		heap[cur] = heap[child];
+		cur = child; child <<= 1;
+	}
+	heap[cur] = e;
+	return res;
+}
+		
