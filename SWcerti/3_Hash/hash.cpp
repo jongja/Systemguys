e1e89982864a25bd8082210cfe771c56da31ccc2
jongja <whwhdgus94@naver.com>
2020-08-10 22:27:16 +0900

    change type unsigned int for overflow

--- SWcerti/3_Hash/hash.cpp
+++ SWcerti/3_Hash/hash.cpp
@@ -2,27 +2,23 @@
  * 
  * Hashing algorithm that has minimum collision
  * TIME COMPLEXITY : O(1)
- *
+ * 
  */
 
 #include <iostream>
 using namespace std;
 
-#define HASH_SIZE 10000
+#define HASH_SIZE 1000000
 #define PRIME_NUM 23
 #define	PRIME_NUM2 29
 
-int hasing(char* str) { // hasing function for string
-	long long key = 0, p = 1;
+unsigned int hasing(char* str) { // hasing function for string
+	unsigned key = 0, p = 1;
 	for (int i = 0; str[i]; i++) {
 		key += str[i] * p;
 		p *= PRIME_NUM;
 	}
 	return key % HASH_SIZE;
-}
-
-int hasing(int val) { // hasing function for integer value (NOT IMPLEMENTED)
-	return ((val % PRIME_NUM) * (val % PRIME_NUM2)) % HASH_SIZE;
 }
 
 int main(void) {
